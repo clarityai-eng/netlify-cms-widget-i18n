c904b9c1c5559b90c904fd2fac79d4a7e704a80d
Arturo Galan <arturo.galan@clarity.ai>
2021-01-14 10:53:31 +0100

    gridEditor

--- src/i18nEditorControl.js
+++ src/i18nEditorControl.js
@@ -19,23 +19,49 @@ import PropTypes from 'prop-types';
        value,
        onChange,
        classNameWrapper,
+       field
      } = this.props;
-     console.log(this.props);
+    //  console.log(this.props);
     //  const collectionProps = collection._root.entries;
-
+     const fieldName = field._root.entries[0][1];
+     const stateValue = JSON.parse(value || '{}');
+     console.log(fieldName, stateValue);
      return (
        <section>
        <h1>i18n JSON editor</h1>
        <table>
-          {[1,2,3].map(column => <tr key={column}>{column}</tr>)}
+         {
+           Object.keys(stateValue).map(element => <tr key={element}>
+             <td>
+             <input
+              type="text"
+              id={element}
+              className={classNameWrapper}
+              value={element}
+              onChange={e => onChange(e.target.value)}
+              />
+              </td>
+             <td>
+             <input
+              type="text"
+              id={`${element}-value`}
+              className={classNameWrapper}
+              value={stateValue[element]}
+              onChange={e => onChange(e.target.value)}
+              />
+              </td>
+              <td><button>Add row</button></td>
+           </tr>)
+         }
+          {/* {[].map(column => <tr key={column}>{column}</tr>)} */}
       </table>
-       <input
+       {/* <input
          type="text"
          id={forID}
          className={classNameWrapper}
          value={value || ''}
          onChange={e => onChange(e.target.value)}
-       />
+       /> */}
       </section>       
      );
    }

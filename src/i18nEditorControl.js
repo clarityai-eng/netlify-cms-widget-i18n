b8fcdbcbecc61c2593386a6c6f3f5045ee299979
Arturo Galan <arturo.galan@clarity.ai>
2021-01-15 01:05:57 +0100

    validation remove file and edit

--- src/i18nEditorControl.js
+++ src/i18nEditorControl.js
@@ -16,6 +16,7 @@ import 'handsontable/dist/handsontable.full.css';
             name: 'Insert row above this one (custom name)'
           },
           'row_below': {},
+          'remove_row': {},
           // 'separator': Handsontable.plugins.ContextMenu.SEPARATOR,
           // 'clear_custom': {
           //   name: 'Clear all cells (custom)',
@@ -29,6 +30,29 @@ import 'handsontable/dist/handsontable.full.css';
       //     console.log(changes)
 
       // }
+    };
+    this.state = {isInError: false};
+    this.errorDesc = [];
+    this.lastChanges = [];
+    this.isValid = ()=>{
+      // Do internal validation
+      this.errorDesc = [];
+      console.log('last changesssss:',this.lastChanges);
+      const {index,colName,oldValue,newValue,row} = this.lastChanges;
+      if (row) {
+        if (!row.value) {
+          this.errorDesc.push(`Value cannot be null in row ${index + 1}`);
+        }
+        if (!row.key) {
+          this.errorDesc.push(`Key cannot be null in row ${index + 1}`);
+        }
+        if (this.errorDesc.length){
+          this.setState({isInError: true});
+        } else {
+          this.setState({isInError: false});
+        }
+      }
+      return this.errorDesc.length ? { error: 'Your error message.' } : true;
     };
   }
 
@@ -44,10 +68,6 @@ import 'handsontable/dist/handsontable.full.css';
      value: '',
    }
 
-  isValid(){
-    // Do internal validation
-    return { error: 'Your error message.' };
-  };
   render() {
     const {
       forID,
@@ -56,39 +76,76 @@ import 'handsontable/dist/handsontable.full.css';
       classNameWrapper,
       field
     } = this.props;
-    console.log(field.get('fields'),field.get('entries'));
     //  const collectionProps = collection._root.entries;
     const fieldName = field._root.entries[0][1];
-    const stateValue = JSON.parse(value || '{}');
+    let stateValue = {};
+    debugger;
+    if (typeof value === 'string') {
+      const tempObj = JSON.parse(value || '{}');
+      stateValue = Object.entries(tempObj).map(([key, value]) => ({key: key || '', value}));
+    }
+    if (typeof value === 'object' && value !== null) {
+      // When coming from file git read, the value Object has some entries that we dont want to map
+      if(value._root) {
+        stateValue = value._root.entries.map(([key, value]) => ({key: key || '', value}));
+      // When coming from updates in the table component, we map all the entries
+      } else {
+        stateValue = Object.entries(value).map(([key, value]) => ({key: key || '', value}));
+      }
+    }
+    // When is not valid, it can be for duplicate keys, the value remains as an array of objects
+    // If we convert to an object, the we lose the duplicate keys, but we want the user to be able
+    // to edit it
+    if (Array.isArray(value)) {
+      stateValue = value;
+    }
     console.log(fieldName, stateValue);
-    
-    
-    const rows = Object.entries(stateValue).map(([key, value]) => ({key: key || '', value}));
+    // const rows = Object.entries(stateValue).map(([key, value]) => ({key: key || '', value}));
     
     const manageChange = (data) => {
       if(data && data[0]) {
         let index,colName,oldValue,newValue;
         [index,colName,oldValue,newValue] = data[0];
-        rows[index][colName] = newValue;
-        const stateValueChanged = {};
-        for (var i=0; i < rows.length; i++) {
-          stateValueChanged[rows[i].key || ''] = rows[i].value;
+        this.lastChanges = {index,colName,oldValue,newValue,row: stateValue[index]};
+        console.log('saved lastchanges',this.lastChanges)
+        stateValue[index][colName] = newValue;
+        if (this.isValid()) {
+          let finalObjectValue = {};
+          for (var i=0; i < stateValue.length; i++) {
+            finalObjectValue[stateValue[i].key || ''] = stateValue[i].value;
+          }
+          onChange(finalObjectValue)
+        } else {
+          onChange(stateValue)
         }
-        onChange(JSON.stringify(stateValueChanged))
       }
     }
-
+    const manageRemoveRow = (index,amount,rows) => {
+      console.log('removing', index, rows)
+      // When removing isValid, so we transform into object and call onChange
+      // TODO if is the last row then isValid = false?
+      let finalObjectValue = {};
+      for (var i=0; i < stateValue.length; i++) {
+        finalObjectValue[stateValue[i].key || ''] = stateValue[i].value;
+      }
+      onChange(finalObjectValue)
+    }
+    const {isInError} = this.state;
+    const errorDesc = this.errorDesc;
     return (
       <section>
+      {isInError && errorDesc.map(error => <tr key={error}>{error}</tr>)}
+
       <div id="hot-app">
         <HotTable
           settings={this.hotSettings}
-          data={rows} 
+          data={stateValue} 
           colHeaders={true} 
           rowHeaders={true} 
           width="800" height="300" 
           licenseKey="non-commercial-and-evaluation" 
           afterChange={manageChange}
+          afterRemoveRow={manageRemoveRow}
         >
         <HotColumn title="Key" data="key"/>
         <HotColumn title="Value" data="value"/>

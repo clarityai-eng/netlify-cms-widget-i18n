4a2900476bb762920707db9972beb62fcccb6b68
Arturo Galan <arturo.galan@clarity.ai>
2021-01-19 00:12:31 +0100

    validation point

--- src/i18nEditorControl.js
+++ src/i18nEditorControl.js
@@ -8,6 +8,7 @@ import './i18nEditorControl.css';
 import { Map } from 'immutable';
 
  export default class i18nEditorControl extends React.Component {
+
   constructor(props) {
     super(props);
     this.hotSettings = {
@@ -25,10 +26,16 @@ import { Map } from 'immutable';
       },
     };
     this.state = {isInError: false};
+    this.hasToValidateFlag = false;
+    this.wasInvalidFlag = false;
+
+    this.duplicatedKeys = {};
+    this.duplicatedCallbacks = {};
     this.errorDesc = [];
     this.rowsKeys = {};
     this.lastChanges = [];
-    this.stateValue;
+    this.stateValue = [];
+    this.stateRemovedValue;
     const getErrorRowOrCreate = (errorsArray, index)=> {
       let found = getErrorRow(errorsArray, index);
       if (!found) {
@@ -40,11 +47,12 @@ import { Map } from 'immutable';
     const getErrorRow = (errorsArray, index)=> {
       return errorsArray.find((el)=> el.index === index);
     }
-    const initializeRowsKeys = ()=> {
-      this.stateValue.forEach((row, index)=> this.rowsKeys[row.key] = [index])
-    }
+
     this.isValid = ()=>{
-      return this.errorDesc.length ? { error: 'Your error message.' } : true;
+      debugger;
+      console.log('is valid with', Object.keys(this.duplicatedKeys));
+      return Object.keys(this.duplicatedKeys).length
+      // return this.errorDesc.length ? { error: 'Your error message.' } : true;
     };
     this.checkLastChangeIsValid = () => {
       const {index,colName,oldValue,newValue} = this.lastChanges;
@@ -58,18 +66,17 @@ import { Map } from 'immutable';
           if (!newValue) {
             foundErrors.push('KEY_NOT_NULL');
           } else {
-            if (!this.rowsKeys.length) initializeRowsKeys();
             const existingKey = this.rowsKeys[newValue];
             //hasDuplicate tiene que devolver el index del duplicate
             if(existingKey) {
               if (existingKey.length === 1) {
                 const existingKeyIndex = existingKey[0];
-                const found = this.errorDesc.find((el)=> el.index === existingKeyIndex);
+                let found = this.errorDesc.find((el)=> el.index === existingKeyIndex);
                 if (!found) {
-                  found = {duplicateIndex, key: existingKeyIndex, errors: []};
-                  errorsArray.push(found);
+                  found = {existingKeyIndex, key: newValue, errors: []};
+                  this.errorDesc.push(found);
                 }
-                found.push('KEY_NOT_NULL');
+                found.errors.push('KEY_NOT_NULL');
               }
 
               foundErrors.push('KEY_DUPLICATED');
@@ -124,6 +131,10 @@ import { Map } from 'immutable';
    }
 
   render() {
+    const initializeRowsKeys = ()=> {
+      this.stateValue.forEach((row, index)=> this.rowsKeys[row.key] = [index])
+      console.log('initialized key map', Object.keys(this.rowsKeys).length)
+    }
     const {
       forID,
       value,
@@ -143,41 +154,83 @@ import { Map } from 'immutable';
       // When coming from file git read, the value Object has some entries that we dont want to map
       if(value._root) {
         this.stateValue = value._root.entries.map(([key, value]) => ({key: key || '', value}));
+        if (!this.rowsKeys.length) initializeRowsKeys();
       // When coming from updates in the table component, we map all the entries
-      } else {
+      } 
+      else {
         this.stateValue = Object.entries(value).map(([key, value]) => ({key: key || '', value}));
       }
     }
+
     // When is not valid, it can be for duplicate keys, the value remains as an array of objects
     // If we convert to an object, the we lose the duplicate keys, but we want the user to be able
     // to edit it
-    if (Array.isArray(value)) {
-      this.stateValue = value;
+    // if (Array.isArray(value)) {
+    //   this.stateValue = value;
+    //   if (!this.rowsKeys.length) initializeRowsKeys();
+    // }
+    const keyValidator = (value, callback) => {
+      if (!this.hasToValidateFlag) {
+        callback(this.wasInvalidFlag ? false : true);
+        return;
+      }
+      if (this.hasToValidateFlag && this.rowsKeys[value]) {
+        debugger;
+        this.duplicatedKeys[value] = (this.duplicatedKeys[value] || 0) + 1;
+        this.duplicatedCallbacks[value] 
+          ? this.duplicatedCallbacks[value].push(callback) 
+          : this.duplicatedCallbacks[value] = [callback]
+        callback(false);
+      } else {
+        callback(true);
+      }
+    }
+    const substractDuplicateKey = (key)=> {
+      let duplicatedRow = this.duplicatedKeys[key];
+      if (duplicatedRow) {
+        if (duplicatedRow === 1) {
+          debugger;
+          delete this.duplicatedKeys[key];
+          this.duplicatedCallbacks[key].forEach((callback)=> callback(true));
+        } else {
+          this.duplicatedKeys[key] = this.duplicatedKeys[key] - 1;
+        }
+      } else {
+        delete this.rowsKeys[key]
+      }
+    }
+    const handleAfterValidate = (data)=> {
+      // debugger;
+    }
+    const handleBeforeChange = (data)=> {
+      let index,colName,oldValue,newValue;
+      [index,colName,oldValue,newValue] = data[0];      
+      this.hasToValidateFlag = oldValue !== newValue;
+      this.wasInvalidFlag = !!this.duplicatedKeys[oldValue];
     }
     const handleChange = (data) => {
-      debugger;
       if(data && data[0]) {
         let index,colName,oldValue,newValue;
         [index,colName,oldValue,newValue] = data[0];
-        this.lastChanges = {index,colName,oldValue,newValue,row: this.stateValue[index]};
-        console.log('saved lastchanges',this.lastChanges)
-        // this.stateValue[index][colName] = newValue;
-        onChange(this.stateValue)
-        if (this.checkLastChangeIsValid()) {
-          let finalObjectValue = {};
-          for (var i=0; i < this.stateValue.length; i++) {
-            finalObjectValue[this.stateValue[i].key || ''] = this.stateValue[i].value;
+        if (colName === 'key') {
+          if(oldValue && oldValue !== newValue) {
+            substractDuplicateKey(oldValue);
           }
-          onChange(finalObjectValue)
-        } 
+          debugger;
+          if (newValue) {
+            this.rowsKeys[newValue] = [index];
+          }
+        }
       }
+      console.log('dupe',this.duplicatedKeys, 'rosKeys', this.rowsKeys);
     }
-    const handleRemoveRow = (index,amount,rows) => {
-      // remove the possible errors from that index
-      this.errorDesc = this.errorDesc.filter((el)=> el.index !== index && el.errors.length > 0)
-      setTimeout(()=> this.forceUpdate(), 500);
-      // TODO update indexes of errors and keyMap
-      // this.setState({isInError: true});
+    const handleBeforeRemoveRow = (index,amount,rows,source) => {
+      this.stateRemovedValue = [];
+      rows.forEach((removedRowIndex)=> {
+        const rowToRemove = this.stateValue[removedRowIndex]
+        substractDuplicateKey(rowToRemove.key)
+      })
+      console.log('dupe',this.duplicatedKeys, 'rosKeys', this.rowsKeys);
     }
     const {isInError} = this.state;
     const errorDesc = this.errorDesc;
@@ -199,10 +252,11 @@ import { Map } from 'immutable';
           rowHeaders={true} 
           licenseKey="non-commercial-and-evaluation" 
           afterChange={handleChange}
-          afterRemoveRow={handleRemoveRow}
-
+          beforeChange={handleBeforeChange}
+          beforeRemoveRow={handleBeforeRemoveRow}
+          afterValidate={handleAfterValidate}
         >
-        <HotColumn title="Key" data="key"/>
+        <HotColumn title="Key" data="key" validator={keyValidator}/>
         <HotColumn title="Value" data="value"/>
         </HotTable>
       </div>

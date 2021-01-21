71938401e0d6c87178ab6847ed376d8b89f199b8
Arturo Galan <arturo.galan@clarity.ai>
2021-01-21 16:11:32 +0100

    add duplicates validation

--- src/i18nEditorControl.js
+++ src/i18nEditorControl.js
@@ -15,7 +15,7 @@ import { Map } from 'immutable';
     this.hotSettings = {
       data: Handsontable.helper.createSpreadsheetData(5, 5),
       colHeaders: true,
-      colWidths: [300, 300],
+      colWidths: [300, 400],
       comments: true,
       // viewportRowRenderingOffset: 70,
       contextMenu: {
@@ -30,8 +30,8 @@ import { Map } from 'immutable';
     };
     this.hotTableComponent = React.createRef();
     this.stateValue = [];
+    this.stateKeysCount = {};
     this.stateValidations = [];
-    // this.stateHaveBeenPublished = false;
   }
 
    static propTypes = {
@@ -46,44 +46,109 @@ import { Map } from 'immutable';
    }
 
    isValid = ()=> {
-     return !this.stateValidations.length;
+     return this.checkNoErrors();
    }
+   convertToFlatArray = (mainObj)=> {
+    let flattenedArray = [];
+    const flatten = (keyToFlatten, path)=> {
+      for (const key in keyToFlatten) {
+        const cumulatePath = `${path ? path + '.' : ''}${key}`;
+        if (typeof keyToFlatten[key] === 'object') {
+          flatten(keyToFlatten[key], cumulatePath)
+        } else {
+          flattenedArray.push({ key: cumulatePath, value: keyToFlatten[key]})
+          this.stateKeysCount[cumulatePath] = 1;
+        }
+      }
+    }
+    flatten(mainObj)
+      return flattenedArray;
+    }
+    checkDuplicates = (key)=> {
+      return !!this.stateKeysCount[key];
+    }
+    addKeyCount = (key) => {
+      this.stateKeysCount[key] = this.stateKeysCount[key] ? this.stateKeysCount[key] + 1 : 1;
+    }
+    setKeyCount = (key, count)=> {
+      this.stateKeysCount[key] = count;
+    }
+    checkNoErrors = () => {
+      return !Object.values(this.stateKeysCount).some((el)=> el > 1)
+    }
+    removeKeyCount = (key)=> {
+      if (this.stateKeysCount[key] === 1) {
+        delete this.stateKeysCount[key];
+      } else if (this.stateKeysCount[key] > 1) {
+        this.stateKeysCount[key] = this.stateKeysCount[key] - 1;
+      } else {
+        console.warn('Trying to remove no existing key: ', key);
+      }
+    }
+
    componentDidMount() {
+    const updateDataInNetlify = () => {
+      //if no errors then call to update to be able to publish changes
+      
+      if (this.checkNoErrors()) {
+        console.log('No errors, calling netlify')
+        let finalObjectValue = {};
+        for (var i=0; i < this.stateValue.length; i++) {
+          finalObjectValue[this.stateValue[i].key || ''] = this.stateValue[i].value;
+        }
+        console.log('llamada a change', finalObjectValue)
+        this.props.onChange(finalObjectValue)
+      }
+    }
+
     if(this.hotTableComponent.current) {
       this.hotTableComponent.current.hotInstance.addHook('afterChange', (changes, source)=> {
-        //after every change, run validation on the "0 column"
-        const instance = this.hotTableComponent.current.hotInstance;
-        this.stateValidations = [];
-        let keyColumnRowsArray = instance.getDataAtCol(0);
-        keyColumnRowsArray.forEach((value, row)=> {
-          let data = Object.assign([], keyColumnRowsArray);
-          let index = data.indexOf(value);
-          data.splice(index, 1);
-          let second_index = data.indexOf(value);
-          let cell= instance.getCellMeta(row, 0);
-          if (index > -1 && second_index > -1 && !(value == null || value === '')) {
-              cell.valid = false
-              cell.comment = { value: 'No Duplicate Value allowed !!!'}
-              this.stateValidations.push({rowIndexes: [index, second_index], error: 'DUPLICATED'});
-          } else {
-              cell.valid = true
-              cell.comment = ''
+        // source -> ['edit', 'loadData']
+        let index,colName,oldValue,newValue;
+        changes && ([index,colName,oldValue,newValue] = changes[0]);
+        if (changes && source === 'edit' && colName === 'key' && oldValue !== newValue) {
+          //after every change, run validation on the "0 column"
+          const instance = this.hotTableComponent.current.hotInstance;
+          const keyIndexes = [];
+          let keyColumnRowsArray = instance.getDataAtCol(0);
+          keyColumnRowsArray.forEach((key, index)=> {
+            if (key === newValue) {
+              keyIndexes.push(index);
+            }
+          });
+          console.log(keyIndexes);
+          const keyAlreadyExists = keyIndexes.length > 1
+          if (keyAlreadyExists) {
+            keyIndexes.forEach((duplicatedKeyIndex)=> {
+              let cell= instance.getCellMeta(duplicatedKeyIndex, 0);
+              cell.valid = false;
+              cell.comment = { value: 'No Duplicate Value allowed !!!'};
+            })
           }
-        });
-        //force a re-render so the new cell properties show up
-        instance.render()
-        //if no errors then call to update to be able to publish changes
-        console.log(this.stateValidations)
-        console.log(this.state)
-
-        if (!this.stateValidations.length && changes) {
-          let finalObjectValue = {};
-          for (var i=0; i < this.stateValue.length; i++) {
-            finalObjectValue[this.stateValue[i].key || ''] = this.stateValue[i].value;
-          }
-          console.log('llamada a change', finalObjectValue)
-          this.props.onChange(finalObjectValue)
+          this.setKeyCount(newValue, keyIndexes.length);
+          this.removeKeyCount(oldValue);
+          instance.render();
+          updateDataInNetlify();
+          // keyColumnRowsArray.forEach((value, row)=> {
+          //   let data = Object.assign([], keyColumnRowsArray);
+          //   let index = data.indexOf(value);
+          //   data.splice(index, 1);
+          //   let second_index = data.indexOf(value);
+          //   let cell= instance.getCellMeta(row, 0);
+          //   if (index > -1 && second_index > -1 && !(value == null || value === '')) {
+          //       cell.valid = false
+          //       cell.comment = { value: 'No Duplicate Value allowed !!!'}
+          //       this.stateValidations.push({rowIndexes: [index, second_index], error: 'DUPLICATED'});
+          //   } else {
+          //       cell.valid = true
+          //       cell.comment = ''
+          //   }
+          // });
+          // //force a re-render so the new cell properties show up
+          // updateDataInNetlify();
+          // instance.render()
         }
+        
       });
     }
   }
@@ -112,7 +177,7 @@ import { Map } from 'immutable';
     )
     console.log('props',this.props)
     console.log('entry',this.props.entry.get('path'));
-    console.log('raw',this.props.entry.get('raw'));
+    // console.log('raw',this.props.entry.get('raw'));
 
     const fileFolder = this.props.collection.get('folder');
     const filePath = this.props.entry.get('path');
@@ -123,50 +188,21 @@ import { Map } from 'immutable';
     const collectionFieldsArray = JSON.parse(JSON.stringify(this.props.collection.get('fields')));
     const collectioni18EditorWidgetField = collectionFieldsArray.find((el)=> el.widget === 'i18nEditor');
     const JSONFilePropName = collectioni18EditorWidgetField.name;
-    const convertToFlat = (mainObj)=> {
-      let flattenedObj = {};
-      const flatten = (keyToFlatten, path)=> {
-        for (const key in keyToFlatten) {
-          const cumulatePath = `${path ? path + '.' : ''}${key}`;
-          if (typeof keyToFlatten[key] === 'object') {
-            flatten(keyToFlatten[key], cumulatePath)
-          } else {
-            flattenedObj[cumulatePath] = keyToFlatten[key]
-          }
-        }
-      }
-      flatten(mainObj)
-      return flattenedObj;
-    }
-    const convertToFlatArray = (mainObj)=> {
-      let flattenedArray = [];
-      const flatten = (keyToFlatten, path)=> {
-        for (const key in keyToFlatten) {
-          const cumulatePath = `${path ? path + '.' : ''}${key}`;
-          if (typeof keyToFlatten[key] === 'object') {
-            flatten(keyToFlatten[key], cumulatePath)
-          } else {
-            flattenedArray.push({ key: cumulatePath, value: keyToFlatten[key]})
-          }
-        }
-      }
-      flatten(mainObj)
-      return flattenedArray;
-    }
 
     if (typeof value === 'object' && value !== null) {
-        // if(value._root) {
-        //   this.stateValue = Array.from(this.props.value.entries()).map(([key, value]) => ({key: key || '', value}));
-        //   console.log('created initial object')
-        // }
-      const receivedObject = JSON.parse(this.props.entry.get('raw'));
-      // const flattenedObject = convertToFlat(receivedObject[JSONFilePropName]);
-      // this.stateValue = Object.entries(flattenedObject).map(([key, value]) => ({key: key || '', value}));
-      this.stateValue = convertToFlatArray(receivedObject[JSONFilePropName]);
-    } else {
-      // Initialize the table object to have at least 1 empty row
-      if(!this.stateValue.length) {
-        this.stateValue.push({key: '', value: ''})
+      // if(value._root) {
+      //   this.stateValue = Array.from(this.props.value.entries()).map(([key, value]) => ({key: key || '', value}));
+      //   console.log('created initial object')
+      // }
+      if(value._root) {        
+        debugger;
+        const receivedObject = JSON.parse(this.props.entry.get('raw'));
+        this.stateValue = this.convertToFlatArray(receivedObject[JSONFilePropName]);
+      } else {
+        // Initialize the table object to have at least 1 empty row
+        if(!this.stateValue.length) {
+          this.stateValue.push({key: '', value: ''})
+        }
       }
     }
 
@@ -181,7 +217,7 @@ import { Map } from 'immutable';
           <div class="header-info">
           {!filePath && <div><span>You are creating a new file inside: </span><span class="italic">/{fileFolder}</span></div>}
           {!!filePath && <div><span>You are editing: </span><span class="italic">/{filePath}</span></div>}
-          {<div>The name of the prop in the JSON file is: {JSONFilePropName}</div>}
+          {<div>The name of the main prop in the JSON file is: {JSONFilePropName}</div>}
         </div>
         }
         <div id="hot-app">
